bbc793d37ca3f6c266af04c949c518665253d901
Anurag Tomar <anurag.189301052@muj.manipal.edu>
2018-10-11 00:58:45 +0530

    3d_array_doubt

---
+++ array.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author atoma
+ */
+public class array {
+     
+    public static void main(String args[])
+    {
+      int [][][] grid =
+      {
+          {
+              {1, 2, 3, 4},
+              {2, 3, 4},
+              {3, 4, 5}
+              },      
+          {
+              {7, 8, 9 ,10},
+                  {18,19,20},
+                  {22, 21, 14},
+    }   
+          };
+      System.out.println(grid [0][0][0]);
+}
+}
